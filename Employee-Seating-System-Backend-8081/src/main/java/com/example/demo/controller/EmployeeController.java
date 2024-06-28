45fe6533b94b5e41ba3fd6b3032ea696b30d9370
tingliiiii <lily90740@gmail.com>
2024-06-28 19:26:18 +0800

    update

--- Employee-Seating-System-Backend-8081/src/main/java/com/example/demo/controller/EmployeeController.java
+++ Employee-Seating-System-Backend-8081/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -4,17 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.dto.UpdateSeat;
 import com.example.demo.model.po.Employee;
 import com.example.demo.model.response.ApiResponse;
 import com.example.demo.model.response.StatusMessage;
